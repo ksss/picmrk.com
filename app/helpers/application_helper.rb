a712412e45f88ef4d6a49a7777147c6a425fa0d9
ksss <co000ri@gmail.com>
2015-07-14 22:28:36 +0900

    Use presigned_url instead of public_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,7 +40,13 @@ module ApplicationHelper
       src = "localhost:3000/tori/#{tori_file.name}"
       "#{Rails.application.config.kanoko_host}#{Kanoko.path_for(*args, src)}"
     when Tori::Backend::S3
-      src = tori_file.public_url.to_s.sub(%r{https?://}, '')
+      signer = Aws::S3::Presigner.new
+      src = signer.presigned_url(
+        :get_object,
+        bucket: Tori.config.backend.bucket,
+        key: tori_file.name
+      ).to_s.sub(%r{https?://}, '')
+#      src = tori_file.public_url.to_s.sub(%r{https?://}, '')
       "#{Rails.application.config.kanoko_host}#{Kanoko.path_for(*args, src)}"
     end
   end
