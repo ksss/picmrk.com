833ce992171f0676e1f98a907a5ebc4bc58db4dc
ksss <co000ri@gmail.com>
2015-07-17 10:33:14 +0900

    Unicorn tempfile will close after GC.start
    So, Let's GC.disable :smiling_imp:

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -2,3 +2,21 @@ listen "/tmp/unicorn.picmrk.com.sock"
 worker_processes 4
 pid "tmp/pids/unicorn.pid"
 preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill(:QUIT, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+  GC.disable
+end
